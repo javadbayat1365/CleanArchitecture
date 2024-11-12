664cad17349f466064ee58190ab0fd174bab6e65
javad <javad@DESKTOP-UJE2PCK>
2024-11-12 08:45:42 +0330

    Added OperationResult, IUserManager in Features, RegisterUserCommand

---
+++ Application/Common/OperationResult.cs
@@ -0,0 +1,41 @@
+﻿namespace Application.Common;
+
+public interface IOperatoinResult
+{
+    bool IsSuccess { get; set; }
+    bool IsNotFound { get; set; }
+    List<KeyValuePair<string,string>> ErrorMessages { get; set; }
+}
+
+public class OperationResult<TResult> : IOperatoinResult
+{
+    public TResult? Result { get; set; }
+    public bool IsSuccess { get; set ; }
+    public bool IsNotFound { get; set ; }
+    public List<KeyValuePair<string, string>> ErrorMessages { get; set; } = new();
+
+    public static OperationResult<TResult> SuccessResult(TResult result)
+    {
+        return new OperationResult<TResult> { Result = result, IsSuccess = true };
+    }
+    public static OperationResult<TResult> FailureResult(string propertyName,string message)
+    {
+        var result = new OperationResult<TResult>()
+        {
+            Result = default
+        };
+        result.ErrorMessages.Add(new KeyValuePair<string, string>(propertyName,message));
+        return result;
+    }
+
+    public static OperationResult<TResult> NotFoundResult(string propertyName, string message)
+    {
+        var result = new OperationResult<TResult>()
+        {
+            Result = default,
+            IsNotFound = true
+        };
+        result.ErrorMessages.Add(new KeyValuePair<string, string>(propertyName, message));
+        return result;
+    }
+}
---
+++ Application/Features/User/Commands/Register/RegisterUserCommand.cs
@@ -0,0 +1,14 @@
+﻿using Application.Common;
+using Mediator;
+
+namespace Application.Features.User.Commands.Register;
+
+public record RegisterUserCommand(
+    string FirstName,
+    string LastName,
+    string UserName,
+    string Email,
+    string PhoneNumber,
+    string Password,
+    string RepeatPasword
+    ):IRequest<OperationResult<bool>>;
