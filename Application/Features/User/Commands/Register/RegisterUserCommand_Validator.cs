ed4eee390be705cbda3049a36bc43fb36d4caef9
javad bayat <39430951+javadbayat1365@users.noreply.github.com>
2024-11-15 19:29:19 +0330

    create ApplicationValidationExtensions, ApplicationIdentityExtensions, RegisterUserCommandHandler

--- Application/Features/User/Commands/Register/RegisterUserCommand_Validator.cs
+++ Application/Features/User/Commands/Register/RegisterUserCommand_Validator.cs
@@ -1,5 +1,17 @@
-﻿namespace Application.Features.User.Commands.Register;
+﻿using FluentValidation;
 
-public class RegisterUserCommand_Validator
-{ 
+namespace Application.Features.User.Commands.Register;
+
+public class RegisterUserCommand_Validator :AbstractValidator<RegisterUserCommand>
+{
+    public RegisterUserCommand_Validator()
+    {
+        RuleFor(x => x.Email).NotEmpty().EmailAddress();
+        RuleFor(x => x.PhoneNumber).NotEmpty();
+        RuleFor(x => x.FirstName).NotEmpty();
+        RuleFor(x => x.LastName).NotEmpty();
+        RuleFor(x => x.UserName).NotEmpty();
+        RuleFor(x => x.Password).Equal(x => x.RepeatPasword).NotEmpty();
+
+    }
 }
