0f61837f960ca720fb754ce34eca40218569d0a5
javad bayat <39430951+javadbayat1365@users.noreply.github.com>
2024-11-15 19:29:19 +0330

    create ApplicationValidationExtensions, ApplicationIdentityExtensions, RegisterUserCommandHandler

---
+++ Application/Features/User/Commands/Register/RegisterUserCommandHandler.cs
@@ -0,0 +1,31 @@
+﻿using Application.Common;
+using Application.Contracts.User;
+using Application.Extensions;
+using Mediator;
+
+namespace Application.Features.User.Commands.Register;
+
+internal class RegisterUserCommandHandler(IUserManager userManager) : IRequestHandler<RegisterUserCommand, OperationResult<bool>>
+{
+
+    public async ValueTask<OperationResult<bool>> Handle(RegisterUserCommand request, CancellationToken cancellationToken)
+    {
+        var validator = new RegisterUserCommand_Validator();
+        var validationResult = validator.Validate(request);
+
+        if (!validationResult.IsValid)
+            return OperationResult<bool>.FailureResult(validationResult.Errors.ConvertToKeyValuepair());
+
+        var userCreateResult = await userManager.CreateAsync(
+            new Domain.Entities.User.UserEntity(
+                request.FirstName,
+                request.LastName,
+                request.UserName,
+                request.Email) { PhoneNumber = request.PhoneNumber }, cancellationToken);
+
+        if (userCreateResult.Succeeded)
+            return OperationResult<bool>.SuccessResult(true);
+
+        return OperationResult<bool>.FailureResult(userCreateResult.Errors.ConvertToKeyValuePair());
+    }
+}
