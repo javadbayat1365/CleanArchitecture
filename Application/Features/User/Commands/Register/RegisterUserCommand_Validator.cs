be3c7aa600e0b3fb425713ddd486aa5c3ae2dc24
javad <javad@DESKTOP-UJE2PCK>
2024-12-01 09:49:09 +0330

    change registerusercommand validation add xunit test for application

--- Application/Features/User/Commands/Register/RegisterUserCommand_Validator.cs
+++ Application/Features/User/Commands/Register/RegisterUserCommand_Validator.cs
@@ -7,11 +7,18 @@ public class RegisterUserCommand_Validator :AbstractValidator<RegisterUserComman
     public RegisterUserCommand_Validator()
     {
         RuleFor(x => x.Email).NotEmpty().EmailAddress();
-        RuleFor(x => x.PhoneNumber).NotEmpty();
+        RuleFor(x => x.PhoneNumber)
+            .NotEmpty()
+            .Must(x => int.TryParse(x,out var val) && val>0);
+            //.Matches(@"^\?[1-9]\d{1,14}$");//12345678 //TODO Handle PhoneNumber Validation
         RuleFor(x => x.FirstName).NotEmpty();
         RuleFor(x => x.LastName).NotEmpty();
         RuleFor(x => x.UserName).NotEmpty();
-        RuleFor(x => x.Password).Equal(x => x.RepeatPasword).NotEmpty();
+        RuleFor(x => x.Password).NotEmpty();
+        RuleFor(x => x.RepeatPasword)
+            .NotEmpty()
+            .Equal(x => x.Password)
+            .WithMessage("Password and Repeat Password Are Not Same!");
 
     }
 }
---
+++ Tests/Application.Tests/Extensions/AppliactionTestExtensions.cs
@@ -0,0 +1,16 @@
+﻿using Application.Common;
+using Xunit.Abstractions;
+using Xunit.Sdk;
+
+namespace Application.Tests.Extensions;
+
+public static class AppliactionTestExtensions
+{
+    public static void WriteLineOperationResultErrors<TResult>(this ITestOutputHelper outputHelper,OperationResult<TResult> operationResult)
+    {
+        foreach (var item in operationResult.ErrorMessages)
+        {
+            outputHelper.WriteLine($"PropertyName: {item.Key}, Message:{item.Value}");
+        }
+    }
+}
