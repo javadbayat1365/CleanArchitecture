10682af3291ae87d771ddc27679aa6bec38b5cc2
javad bayat <39430951+javadbayat1365@users.noreply.github.com>
2024-12-14 06:00:44 +0330

    validation with reflection

---
+++ Application/Common/Validation/IValidatableModel.cs
@@ -0,0 +1,8 @@
+﻿using FluentValidation;
+
+namespace Application.Common.Validation;
+
+public interface IValidatableModel<TRequestApplicationModel> where TRequestApplicationModel : class
+{
+    IValidator<TRequestApplicationModel> Validate(ValidationModelBase<TRequestApplicationModel> validator);
+} 
---
+++ Application/Common/Validation/ValidationModelBase.cs
@@ -0,0 +1,11 @@
+﻿using FluentValidation;
+
+namespace Application.Common.Validation;
+
+/// <summary>
+/// Marker Validator class
+/// </summary>
+/// <typeparam name="TRequestModel">A reqeust model in form of command and query</typeparam>
+public class ValidationModelBase<TRequestModel>:AbstractValidator<TRequestModel> where TRequestModel : class
+{
+}
