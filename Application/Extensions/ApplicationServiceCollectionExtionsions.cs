1eec6106bee8e18d3541e3905e8d0e557c9b2516
javad bayat <39430951+javadbayat1365@users.noreply.github.com>
2024-12-14 06:00:44 +0330

    validation with reflection

---
+++ Application/Extensions/ApplicationServiceCollectionExtionsions.cs
@@ -0,0 +1,36 @@
+﻿using Application.Common.Validation;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Application.Extensions;
+
+public static class ApplicationServiceCollectionExtionsions
+{
+    public static IServiceCollection RegisterApplicationValidators(this IServiceCollection services)
+    {
+        var validationTypes = AppDomain.CurrentDomain.GetAssemblies()
+            .SelectMany(c => c.GetExportedTypes())
+            .Where(w => w.GetInterfaces().Any(i => i.IsGenericType && i.GetGenericTypeDefinition() == typeof(IValidatableModel<>)));
+
+        foreach (var validationType in validationTypes)
+        {
+            var biggestConstrunctorLength = validationType.GetConstructors()
+                .OrderByDescending(x => x.GetParameters().Length).First().GetParameters().Length;
+
+            var requestModel = Activator.CreateInstance(validationType, new object[biggestConstrunctorLength]);
+
+            if (requestModel is null)
+                continue;
+
+            var requstMethodInfo = validationType.GetMethod(nameof(IValidatableModel<object>.Validate));
+
+            var validationModelBase = Activator.CreateInstance(typeof(ValidationModelBase<>).MakeGenericType(validationType));
+
+            if (validationModelBase is null)
+                continue;
+
+            var validator = requstMethodInfo.Invoke(requestModel, new[] { validationModelBase});
+        }
+
+        return services;
+    }
+}
