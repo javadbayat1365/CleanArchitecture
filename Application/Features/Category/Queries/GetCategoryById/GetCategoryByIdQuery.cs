b6af6d3f20838ac40025385fffd730128a63baf1
javad bayat <39430951+javadbayat1365@users.noreply.github.com>
2024-12-27 22:49:48 +0330

    Add GetCategoryByIdQuery

---
+++ Application/Features/Category/Queries/GetCategoryById/GetCategoryByIdQuery.cs
@@ -0,0 +1,15 @@
+﻿using Application.Common;
+using Application.Common.Validation;
+using FluentValidation;
+using Mediator;
+
+namespace Application.Features.Category.Queries.GetCategoryById;
+
+public record GetCategoryByIdQuery(Guid CategoryId) : IRequest<OperationResult<GetCategoryByIdQueryResult>>, IValidatableModel<GetCategoryByIdQuery>
+{
+    public IValidator<GetCategoryByIdQuery> Validate(ValidationModelBase<GetCategoryByIdQuery> validator)
+    {
+        validator.RuleFor(x => x.CategoryId).NotEmpty();
+        return validator;
+    }
+}
---
+++ Application/Features/Category/Queries/GetCategoryById/GetCategoryByIdQueryHandler.cs
@@ -0,0 +1,18 @@
+﻿using Application.Common;
+using Application.Repositories.Common;
+using Mediator;
+
+namespace Application.Features.Category.Queries.GetCategoryById;
+
+public class GetCategoryByIdQueryHandler(IUnitOfWork unitOfWork) : IRequestHandler<GetCategoryByIdQuery, OperationResult<GetCategoryByIdQueryResult>>
+{
+    public async ValueTask<OperationResult<GetCategoryByIdQueryResult>> Handle(GetCategoryByIdQuery request, CancellationToken cancellationToken)
+    {
+        var category =await unitOfWork.CategoryRepository.GetCategoryByIdAsync(request.CategoryId,cancellationToken);
+        if (category is null)
+            return OperationResult<GetCategoryByIdQueryResult>.NotFoundResult(
+                nameof(GetCategoryByIdQuery.CategoryId),"Category Not Found!"
+                );
+        return OperationResult<GetCategoryByIdQueryResult>.SuccessResult(new GetCategoryByIdQueryResult(category.Id,category.Name));
+    }
+}
