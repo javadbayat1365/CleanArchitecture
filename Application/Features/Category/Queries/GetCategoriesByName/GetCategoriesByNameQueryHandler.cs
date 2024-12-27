e5dddf52c6cddcf8a53058136591396de06b5f3b
javad bayat <39430951+javadbayat1365@users.noreply.github.com>
2024-12-27 22:59:14 +0330

    add GetCategoriesByNameQuery

---
+++ Application/Features/Category/Queries/GetCategoriesByName/GetCategoriesByNameQueryHandler.cs
@@ -0,0 +1,17 @@
+﻿using Application.Common;
+using Application.Repositories.Common;
+using Mediator;
+
+namespace Application.Features.Category.Queries.GetCategoriesByName;
+
+public class GetCategoriesByNameQueryHandler (IUnitOfWork unitOfWork)
+    : IRequestHandler<GetCategoriesByNameQuery, OperationResult<List<GetCategoriesByNameQueryResult>>>
+{
+    public async ValueTask<OperationResult<List<GetCategoriesByNameQueryResult>>> Handle(GetCategoriesByNameQuery request, CancellationToken cancellationToken)
+    {
+        var categories = await unitOfWork.CategoryRepository.GetCategoriesBaseOnNameAsync(request.CategoryName,cancellationToken);
+
+        return OperationResult<List<GetCategoriesByNameQueryResult>>
+            .SuccessResult(categories.Select(c => new GetCategoriesByNameQueryResult(c.Id, c.Name)).ToList());
+    }
+}
