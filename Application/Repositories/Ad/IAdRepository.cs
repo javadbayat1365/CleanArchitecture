2979a31674a5c2edb961d75aadd78778deccb492
javad bayat <39430951+javadbayat1365@users.noreply.github.com>
2025-01-03 14:28:02 +0330

    complete EditAdCommandHandler

--- Application/Repositories/Ad/IAdRepository.cs
+++ Application/Repositories/Ad/IAdRepository.cs
@@ -5,7 +5,7 @@ namespace Application.Repositories.Ad;
 public interface IAdRepository
 {
     Task CreateAdAsync(AdEntitiy adEntitiy,CancellationToken cancellationToken=default);
-    Task<AdEntitiy> GetAdByIdAsync(Guid adId,CancellationToken cancellationToken = default);
+    Task<AdEntitiy?> GetAdByIdAsync(Guid adId,CancellationToken cancellationToken = default);
     Task<List<AdEntitiy>> GetUserAdsAsync(Guid userId,CancellationToken cancellationToken = default);
     Task<List<AdEntitiy>> GetVerifiedAdsAsync(int currentPage, int pageCount,CancellationToken cancellationToken = default);
     
