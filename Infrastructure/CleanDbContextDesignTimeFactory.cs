ca599af8c95da57b7a6dc0095e14bd77593b90e0
Javad Bayat <javadbayat.1365@gmail.com>
2025-01-19 16:11:50 +0330

    Add CleanDbContextDesignTimeFactory And Add Migration

---
+++ Infrastructure/CleanDbContextDesignTimeFactory.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+
+namespace Persistence;
+
+public class CleanDbContextDesignTimeFactory : IDesignTimeDbContextFactory<CleanDbContext>
+{
+    public CleanDbContext CreateDbContext(string[] args)
+    {
+        var optionBuilder = new DbContextOptionsBuilder<CleanDbContext>().UseSqlServer();
+
+        return new CleanDbContext(optionBuilder.Options);
+    }
+}
