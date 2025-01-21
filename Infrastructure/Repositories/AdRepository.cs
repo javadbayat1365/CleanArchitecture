b5aa3b5a124919d790e7b410137ea004258ffd35
Javad Bayat <javadbayat.1365@gmail.com>
2025-01-21 15:07:01 +0330

    Implement UnitOfWork

---
+++ Infrastructure/Repositories/AdRepository.cs
@@ -0,0 +1,42 @@
+﻿using Application.Repositories.Ad;
+using Domain.Entities.Ad;
+using Microsoft.EntityFrameworkCore;
+using Persistence.Repositories.Common;
+
+namespace Persistence.Repositories;
+
+internal class AdRepository(CleanDbContext db) : BaseRepository<AdEntitiy>(db), IAdRepository
+{
+    public async Task CreateAdAsync(AdEntitiy adEntitiy, CancellationToken cancellationToken = default)
+    {
+       await  AddAsync(adEntitiy, cancellationToken);
+    }
+
+    public async Task<AdEntitiy?> GetAdByIdForUpdateAsync(Guid adId, CancellationToken cancellationToken = default)
+    {
+        return await Table.SingleOrDefaultAsync(s => s.Id.Equals(adId),cancellationToken);
+    }
+
+    public async Task<AdEntitiy?> GetAdDetailByIdAsync(Guid adId, CancellationToken cancellationToken)
+    {
+        return await TableNoTracking
+            .Include(s => s.User)
+            .Include(s => s.Location)
+            .Include(s => s.Category)
+            .SingleOrDefaultAsync(s => s.Id.Equals(adId), cancellationToken);
+    }
+
+    public async Task<List<AdEntitiy>> GetUserAdsAsync(Guid userId, CancellationToken cancellationToken = default)
+    {
+        return await TableNoTracking.Where(s => s.UserId.Equals(userId)).ToListAsync(cancellationToken);
+    }
+
+    public async Task<List<AdEntitiy>> GetVerifiedAdsAsync(int currentPage, int pageCount, CancellationToken cancellationToken = default)
+    {
+        return await TableNoTracking
+            .Where(s => s.CurrentState == AdEntitiy.AdState.Approved)
+            .Skip((currentPage-1) * pageCount)
+            .Take(pageCount)
+            .ToListAsync(cancellationToken);
+    }
+}
---
+++ Infrastructure/Repositories/Common/UnitOfWork.cs
@@ -0,0 +1,36 @@
+﻿using Application.Repositories.Ad;
+using Application.Repositories.Category;
+using Application.Repositories.Common;
+using Application.Repositories.Location;
+
+namespace Persistence.Repositories.Common;
+
+internal class UnitOfWork : IUnitOfWork
+{
+    private readonly CleanDbContext _db;
+    public UnitOfWork(CleanDbContext db)
+    {
+        _db = db;
+        LocationRepository = new LocationRepository(db);
+        CategoryRepository = new CategoryRepository(db);
+        AdRepository = new AdRepository(db);
+    }
+    public ILocationRepository LocationRepository { get; }
+
+    public ICategoryRepository CategoryRepository { get; }
+
+    public IAdRepository AdRepository { get; }
+
+    public async Task CommitAsync(CancellationToken cancellationToken = default) => await _db.SaveChangesAsync(cancellationToken);
+    
+
+    public void Dispose()
+    {
+        _db.Dispose();
+    }
+
+    public async ValueTask DisposeAsync()
+    {
+       await _db.DisposeAsync();
+    }
+}
