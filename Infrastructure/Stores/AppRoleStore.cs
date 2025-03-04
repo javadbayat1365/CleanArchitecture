80eaa5e7af920909322b641df346f7de68f9e3e4
Javad Bayat <javadbayat.1365@gmail.com>
2025-03-04 11:33:35 +0330

    add role and user store

---
+++ Infrastructure/Stores/AppRoleStore.cs
@@ -0,0 +1,8 @@
+﻿using Domain.Entities.User;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+
+namespace Persistence.Stores;
+
+public class AppRoleStore(CleanDbContext context, IdentityErrorDescriber? describer=null)
+    :RoleStore<RoleEntity,CleanDbContext,Guid>(context,describer);
