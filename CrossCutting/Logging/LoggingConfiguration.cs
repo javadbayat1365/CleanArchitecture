9b5aa7d4aab0150b775e0cb87378f2cbe0a2267b
javad bayat <39430951+javadbayat1365@users.noreply.github.com>
2025-04-04 19:59:21 +0330

    add Serilog for logging in CrossCuttingConcern layer

---
+++ CrossCutting/Logging/LoggingConfiguration.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.Extensions.Hosting;
+using Serilog;
+using Serilog.Enrichers.Span;
+using Serilog.Exceptions;
+
+namespace CrossCutting.Logging;
+
+public class LoggingConfiguration
+{
+    public static Action<HostBuilderContext, LoggerConfiguration> ConfigureLogger = (context, configuration) => {
+
+        var applicationName = context.HostingEnvironment.ApplicationName;
+
+        configuration
+        .Enrich.WithSpan()
+        .Enrich.WithEnvironmentName()
+        .Enrich.WithMachineName()
+        .Enrich.WithExceptionDetails()
+        .Enrich.WithProperty("ApplicationName: ", applicationName);
+
+        if (context.HostingEnvironment.IsDevelopment())
+        {
+            configuration.WriteTo.Console().MinimumLevel.Information();
+            return;
+        }
+        if (context.HostingEnvironment.IsProduction())
+        {
+            configuration.WriteTo.Console().MinimumLevel.Error();
+            //TODO add SEQ Sink Configuration
+        }
+    };    
+}
