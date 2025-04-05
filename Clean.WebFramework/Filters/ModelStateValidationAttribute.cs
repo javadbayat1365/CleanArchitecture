6fbf3a06441970045c66d732855c91c1db714d7b
Javad Bayat <javadbayat.1365@gmail.com>
2025-04-05 18:23:39 +0330

    add modelstate attribute

---
+++ Clean.WebFramework/Filters/ModelStateValidationAttribute.cs
@@ -0,0 +1,21 @@
+﻿using FluentValidation;
+using Microsoft.AspNetCore.Mvc.Filters;
+
+namespace Clean.WebFramework.Filters;
+
+public class ModelStateValidationAttribute:ActionFilterAttribute
+{
+    public override async Task OnActionExecutionAsync(ActionExecutingContext context, ActionExecutionDelegate next)
+    {
+        foreach (var actionArgument in context.ActionArguments.Values)
+        {
+            var validator = context.HttpContext.RequestServices.GetService(typeof(IValidator<>).MakeGenericType(actionArgument!.GetType()));
+            if(validator is IValidator validatorInstance)
+            {
+                var validationResult = await validatorInstance.ValidateAsync(new ValidationContext<object>(actionArgument));
+                if(!validationResult.IsValid)
+                    validationResult.Errors.ForEach(e => context.ModelState.AddModelError(e.PropertyName,e.ErrorMessage));
+            }
+        }
+    }
+}
--- Clean.WebFramework/Filters/NotFoundAttribute.cs
+++ Clean.WebFramework/Filters/NotFoundAttribute.cs
@@ -1,4 +1,7 @@
-﻿using Microsoft.AspNetCore.Mvc.Filters;
+﻿using Clean.WebFramework.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
 
 namespace Clean.WebFramework.Filters;
 
@@ -6,6 +9,21 @@ public class NotFoundAttribute:ResultFilterAttribute
 {
     public override void OnResultExecuting(ResultExecutingContext context)
     {
-        base.OnResultExecuting(context);
+        if(context.Result is NotFoundObjectResult notFoundObjectResult)
+        {
+            var apiResult = new ApiResult<object>(false,
+               ApiResultStatusCode.NotFound.ToString("G"),
+               ApiResultStatusCode.NotFound,
+               notFoundObjectResult.Value!);
+            context.Result = new JsonResult(apiResult) { StatusCode = StatusCodes.Status404NotFound };
+        }
+
+        if (context.Result is NotFoundResult notFoundResult)
+        {
+            var apiResult = new ApiResult(false,
+               ApiResultStatusCode.NotFound.ToString("G"),
+               ApiResultStatusCode.NotFound);
+            context.Result = new JsonResult(apiResult) { StatusCode = StatusCodes.Status404NotFound };
+        }
     }
 }
